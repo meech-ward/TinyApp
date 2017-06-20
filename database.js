4ef29237255e4219346a77653dea5fad4ba0309e
Sam Meech-Ward <sam@meech-ward.com>
2017-06-20 13:19:55 -0700

    Save a registered user
    When a user uses the registration form to register, save their details to the user database.

--- database.js
+++ database.js
@@ -1,7 +1,21 @@
+const shortid = require('shortid');
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
+};
+
+const userDatabase = {
+  userRandomID: {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur',
+  },
+  user2RandomID: {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk',
+  },
 };
 
 function allURLs() {
@@ -28,3 +42,38 @@ function modifyURL(urlID, longURL) {
   return allURLs();
 }
 exports.modifyURL = modifyURL;
+
+/**
+ * Create or update a user in the database.
+ * @param {* The user's email address} email
+ * @param {* The plain text password} password
+ * @param {* The ID of the user to update, Null to create a new user} userID
+ */
+function saveUser(email, password, _userID) {
+  let userID = _userID;
+  if (!userID) {
+    userID = shortid.generate();
+  }
+
+  const user = {
+    id: userID,
+    email,
+    password,
+  };
+
+  userDatabase[userID] = user;
+
+  console.log(`UserDatabase: ${JSON.stringify(userDatabase)}`);
+
+  delete user.password;
+  return Promise.resolve(user);
+}
+exports.saveUser = saveUser;
+
+function getUser(userID) {
+  const user = userDatabase[userID];
+
+  delete user.password;
+  return Promise.resolve(user);
+}
+exports.getUser = getUser;
--- routes/user-account.js
+++ routes/user-account.js
@@ -1,6 +1,7 @@
 /* eslint no-console:0 */
 const express = require('express');
 const redirect = require('../redirect');
+const database = require('../database');
 
 const router = express.Router();
 
@@ -22,5 +23,22 @@ router.post('/logout', (req, res) => {
 router.get('/register', (req, res) => {
   res.render('user_register');
 });
+
+router.post('/register', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email == '' || password == '') {
+    res.sendStatus(400);
+  }
+
+  database.saveUser(email, password)
+  .then((user) => {
+    res.cookie('user_id', user.id);
+    redirect.redirectToURLs(res);
+  })
+  .catch(() => res.sendStatus(400));
+});
+
 
 module.exports = router;
